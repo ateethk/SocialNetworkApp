04914a2b305571ffb745826e7a8bf812cdfe2347
Ateeth Kosuri <ateethkosuri@Ateeths-MacBook-Pro.local>
2016-09-30 15:56:48 -0700

    Initial comml commit.

---
+++ SocialNetworkApp/SignInVC.swift
@@ -0,0 +1,22 @@
+//
+//  SignInVC.swift
+//  SocialNetworkApp
+//
+//  Created by Ateeth Kosuri on 9/30/16.
+//  Copyright © 2016 Ateeth Kosuri. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
